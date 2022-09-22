2cccdc1282952631fc65f8c33aef4dcac74d2c6c
basikaka <hellochenpro@hotmail.com>
2022-09-22 09:36:27 +0800

    spring101

---
+++ spring-hi/src/test/java/com/hogrider/SpringHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogrider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
